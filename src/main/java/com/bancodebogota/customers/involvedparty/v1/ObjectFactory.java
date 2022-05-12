92d0fb208b1a4c033331683769bec453ad194b8d
fersuares <89793368+fersuares@users.noreply.github.com>
2022-05-12 16:00:41 -0500

    Eliminacion constructor
    Eliminacion del constructor Metodo public ObjectFactory() {   }

--- src/main/java/com/bancodebogota/customers/involvedparty/v1/ObjectFactory.java
+++ src/main/java/com/bancodebogota/customers/involvedparty/v1/ObjectFactory.java
@@ -30,8 +30,6 @@ public class ObjectFactory {
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.bancodebogota.customers.involvedparty.v1
      * 
      */
-    public ObjectFactory() {
-    }
 
     /**
      * Create an instance of {@link SafeInfoType }
